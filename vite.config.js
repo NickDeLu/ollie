fb061e153f0da0848b14826e5f053634a9ccca94
Nick <delunico@sheridancollege.ca>
2023-01-26 01:39:06 -0500

    test env

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 const viteEnv = {};
 Object.keys(process.env).forEach((key) => {
   if (key.startsWith('VITE_')) {
-    viteEnv[`import.meta.env.${key}`] = process.env[key]
+    viteEnv[key] = process.env[key]
   }
 })
 
