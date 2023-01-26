1b5b54fe62775e90f4c6798ab43e82f53c036db3
Nick <delunico@sheridancollege.ca>
2023-01-26 01:02:26 -0500

    adding env support for deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+const viteEnv = {};
+Object.keys(process.env).forEach((key) => {
+  if (key.startsWith('VITE_')) {
+    viteEnv[`import.meta.env.${key}`] = process.env[key]
+  }
+})
+
+
 export default defineConfig({
   plugins: [react()],
+  define: viteEnv
 });
